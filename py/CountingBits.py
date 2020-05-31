a8dbeb860324600171d40c3c16f353d01297a6a7
harshraj22 <harshraj22aug@gmail.com>
2020-06-01 00:02:44 +0530

    updated implementation

--- py/CountingBits.py
+++ py/CountingBits.py
@@ -1,5 +1,17 @@
 class Solution:
     def countBits(self, num: int) -> List[int]:
-        bitCount = lambda x: bin(x).count('1')
+        builtin_popcount = lambda x: bin(x).count('1')
+        return [builtin_popcount(i) for i in range(num+1)]
 
-        return [bitCount(i) for i in range(num+1)]
+""" 
+public:
+    vector<int> countBits(int num) {
+        
+        vector<int> ans;
+        for (int i = 0; i <= num; i++)
+            ans.emplace_back(__builtin_popcount(i));
+        return ans;
+    }
+}
+
+ """

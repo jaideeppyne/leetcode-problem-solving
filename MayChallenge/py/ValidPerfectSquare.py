ccb169077461a3ed98da7ee6459f93a16ab4461e
harshraj22 <harshraj22aug@gmail.com>
2020-06-08 21:23:34 +0530

    added solution to: Valid Perfect Square

---
+++ MayChallenge/py/ValidPerfectSquare.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+    	low, high = 1, num
+    	while low <= high:
+    		mid = (low + high)//2
+    		if mid * mid == num:
+    			return True
+    		elif mid * mid < num:
+    			low = mid + 1
+    		else :
+    			high = mid - 1
+    	return False
+
+
+
+
